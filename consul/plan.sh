a4b316844769e811a91be2b80485ccb80ad9467b
Christopher P. Maher <chris@mahercode.io>
2018-04-25 06:17:40 -0700

    Testing again

--- consul/plan.sh
+++ consul/plan.sh
@@ -4,6 +4,8 @@ pkg_version="1.0.2"
 pkg_maintainer="Christopher Maher<chris@mahercode.io>"
 pkg_license=("MIT")
 pkg_deps=(core/consul)
+pkg_svc_user=root
+pkg_svc_group=root
 
 do_build() {
   return 0
