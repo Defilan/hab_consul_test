83a5646f5e3ba33d8b66e4bc24b2b50e832cab5c
Christopher P. Maher <chris@mahercode.io>
2018-04-25 05:15:37 -0700

    Reverting code to working state for demo

--- consul/plan.sh
+++ consul/plan.sh
@@ -2,11 +2,8 @@ pkg_name=consul
 pkg_origin=defilan
 pkg_version="1.0.2"
 pkg_maintainer="Christopher Maher<chris@mahercode.io>"
-pkg_license=("Apache-2.0")
+pkg_license=("MIT")
 pkg_deps=(core/consul)
-
-pkg_svc_user=root
-pkg_svc_group=root
 
 do_build() {
   return 0
